5560f4d2e689a422ddfd16fd5bd9714e9426c376
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-11-28 16:47:25 -0500

    Added Angular Router

--- gamer-lobby/src/app/app.module.ts
+++ gamer-lobby/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
