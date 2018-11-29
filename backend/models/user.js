518cd088cfeb6c9590021397746e7cbb4651caf8
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-11-29 15:40:15 -0500

    [UserSchema] Updated Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
-const userSchema = new Schema({username: String, level: Number, mode: String, time: String, game: String, status: String });
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String, 
+  level: Number, 
+  mode: String, 
+  time: String, 
+  game: String, 
+  status: String 
+});
+
 module.exports = mongoose.model('userlist', userSchema);
