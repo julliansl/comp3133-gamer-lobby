32d8cbcd5a5bb27d5734898cf2d6627dc7613b09
jaepun <jackyphung.393@gmail.com>
2018-11-29 23:43:18 -0500

    fixed view and added stuff for invite

--- gamer-lobby/src/app/users/users-invite.component.ts
+++ gamer-lobby/src/app/users/users-invite.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../game';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-users-invite',
@@ -9,9 +10,21 @@ import { Game } from '../game';
 export class UsersInviteComponent implements OnInit {
   games: Game[] = []
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  getGameData(): void {
+    this.apiService.get('games')
+      .subscribe(response => {
+        let data = response.json();
+        for (let game in data) {
+          let userObj = Object.assign(new Game(), data[game]);
+          this.games.push(userObj);
+        }
+      });
+  }
 
   ngOnInit() {
+    this.getGameData();
   }
 
 }
