c9eca93a296eaa318eef3d86a0670e4c38e20325
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-11-30 18:10:20 -0500

    [Services] Created Game Service

---
+++ gamer-lobby/src/app/services/game.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Game } from '../game';
+import { Observable, of } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  public games: Game[] = [];
+  private api_schema = 'games';
+
+  constructor(private apiService: ApiService) { }
+
+  getData() {
+    if (this.games.length == 0)
+      this.updateData();
+    return this.games;
+  }
+
+  updateData() {
+    this.games = [];
+    this.retrieveGameData();
+  }
+
+  retrieveGameData(): void {
+    this.get().subscribe(response => {
+        let data = response.json();
+        for (let game in data) {
+          let userObj = Object.assign(new Game(), data[game]);
+          this.games.push(userObj);
+        }
+      });
+  }
+
+  get(): Observable<any> {
+    return this.apiService.get(this.api_schema);
+  }
+
+  create(data = {}) {
+    this.apiService.create(this.api_schema, data);
+  }
+
+  update(data = {}) {
+    this.apiService.update(this.api_schema, data);
+  }
+
+  delete(data = {}) {
+    this.apiService.delete(this.api_schema, data);
+  }
+}
