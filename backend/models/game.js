e8d5e1acfa850d97e38816cde4242f0d5efbd06f
jaepun <jackyphung.393@gmail.com>
2018-11-29 23:46:58 -0500

    added game model

---
+++ backend/models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+    title: String,
+    genre: String,
+    review: String,
+    release: Number,
+    publisher: String,
+    status: String
+});
+
+module.exports = mongoose.model('gamelist', userSchema);
