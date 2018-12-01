00a92b94a40aea9b0a8b245f5ac77acd80091b5c
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-11-30 21:07:25 -0500

    [CSS] Large Styling Update

--- gamer-lobby/src/app/app-routing.module.ts
+++ gamer-lobby/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-{path: '', redirectTo: 'users', pathMatch: 'full'},
-{path:'**', redirectTo: 'users', pathMatch:'full'}];
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path:'**', redirectTo: 'users', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
