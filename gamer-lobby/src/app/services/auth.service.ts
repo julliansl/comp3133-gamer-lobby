c328fa691d88e8a7a9d1d742a498c0d79137eeed
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-12-06 00:00:23 -0500

    [Services][Auth] Updated methods

--- gamer-lobby/src/app/services/auth.service.ts
+++ gamer-lobby/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { AuthenticatedUser } from '../models/authenticated-user';
 import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 
 @Injectable({
@@ -9,19 +11,33 @@ import { typeWithParameters } from '@angular/compiler/src/render3/util';
 })
 export class AuthService {
   authInfo: AuthenticatedUser = null;
-  userAuthenticated: boolean = true;
+  private api_schema = "auth";
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService,
+    private router: Router) { }
 
   authenticated(): boolean {
-    return this.userAuthenticated;
+    return this.authInfo !== null;
   }
 
-  login(username: string, password: string) {
-    
+  login(form: NgForm) {//username: string, password: string) {
+    console.log(JSON.stringify(form.value));
+    //this.authenticateUser({ username: username, password: password });
+    if (this.authInfo !== null) {
+      this.router.navigateByUrl("/admin");
+    }
   }
 
   logout() {
+    this.authInfo = null;
+    if (!this.authInfo) {
+      this.router.navigateByUrl("/users");
+    }
+  }
 
+  authenticateUser(userInfo) {
+    this.apiService.post(this.api_schema, userInfo).subscribe((response) =>{
+      this.authInfo = Object.assign(new AuthenticatedUser(), response)
+    });
   }
 }
