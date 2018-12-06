3c486206fbf454af8766a4b31fa6e2184795ed78
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-12-05 23:54:06 -0500

    [Express] Updated Server Configuration

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const app = express();
 // *********** Include the Api routes ***********
 const userRoutes = require("./routes/users");
 const gameRoutes = require("./routes/games");
+const authRoutes = require("./routes/auth");
 
 // *********** Connect to Mongo  ***********
 console.log('Attempting to connect to mongoose');
@@ -19,7 +20,8 @@ mongoose.connect('mongodb://admin1:admin1@ds223760.mlab.com:23760/user_list', { 
     console.error('App starting error:', err.stack);
   });
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -35,5 +37,6 @@ app.use((req, res, next) => {
 // ******** Setup the Api routes ***********
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
