87a738432013cfdddda613622deac714e0bb157c
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-12-06 18:24:16 -0500

    [Express][API] Users and Games now use auth token on CUD operations
    Read operations are still open for public use

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -4,6 +4,7 @@ const Account = require('../models/account');
 
 const router = express.Router();
 
+// READ ALL
 router.get('', (req, res, next) => {
   res.contentType("application/json");
   console.log('GET: Games Lists');
@@ -14,50 +15,113 @@ router.get('', (req, res, next) => {
   });
 });
 
+// READ
 router.get('/:game', (req, res, next) => {
   res.contentType("application/json");
-  console.log('GET: game by game:' + req.params.game);
-  Game.findOne({game: req.params.game}, (err, game) => {
-    if (err) 
-      throw err;
-    res.send(JSON.stringify(game));
-  });
-})
-
-router.post('', (req, res, next) => {
-  res.contentType("application/json");
-  console.log('UPDATE: game by game: ' + req.body.game);
-  let values = req.body;
-  if (values && Object.keys(values).length > 0) {
-    let game = Game(values);
-    game.save((err, doc) => {
-      if (err)
-        throw err;
-
-      console.log(`Created new game: ${values.game}`);
-      res.send(JSON.stringify(doc));
+  if (req.params.game) {
+    console.log('GET: game by game:' + req.params.game);
+    Game.findOne({ game: req.params.game }, (err, game) => {
+      if (err)  throw err;
+      if (game == null)
+        res.send(JSON.stringify({ message: `The game "${req.params.game}" does not exist in the database` }));
+      else
+        res.send(JSON.stringify(game));
     });
   } else {
-    res.send(JSON.stringify({ message: "Invalid CREATE Query for Game" }));
+    res.send(JSON.stringify({ message: "No game returned due to having no queries" }));
   }
 })
 
-router.put('', (req, res, next) => {
+// CREATE
+router.post('', (req, res, next) => {
   res.contentType("application/json");
-  console.log('UPDATE: game by : ' + req.body.game);
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = token.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.title) {
+          console.log('CREATE: Game by id: ' + values.title);
+          let game = Game(values);
+          game.save((err, doc) => {
+            if (err)
+              throw err;
 
-  game.update({game: req.params.game}, (err, game) => {
-    console.log(`Updated ${game.title} from games collection`);
-  });
+            console.log(`Created new game: ${values.title}`);
+            res.send(JSON.stringify(doc));
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid CREATE Query for Game" }));
+        }
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+  }
 })
 
+// UPDATE
+router.put('', (req, res, next) => {
+  res.contentType("application/json");
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = decodedToken.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.username) {
+          console.log('UPDATE: Game by id: ' + values._id);
+
+          User.findByIdAndUpdate(values._id, (err, user) => {
+            console.log(`Updated ${user.username} from Games collection`);
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid UPDATE Query for Game" }));
+        }
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+  }
+})
+
+// DELETE
 router.delete('', (req, res, next) => {
   res.contentType("application/json");
-  console.log('UPDATE: game by game: ' + req.body.game);
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = decodedToken.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.username) {
+          console.log('DELETE: User by username' + values.username);
+          console.log(values);
 
-  game.findOneAndRemove({game: req.body.game}, (err, game) => {
-    console.log(`Deleted ${game.title} from games collection`);
-  });
+          User.findByIdAndDelete(values._id, (err) => {
+            console.log(`Deleted ${values.title} from Games collection`);
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid DELETE Query for Games" }));
+        }
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+  }
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require('../models/user');
+const Account = require('../models/account');
 
 const router = express.Router();
 
@@ -15,48 +16,107 @@ router.get('', (req, res, next) => {
 
 router.get('/:username', (req, res, next) => {
   res.contentType("application/json");
-  console.log('GET: User by username:' + req.params.username);
-  User.findOne({username: req.params.username}, (err, user) => {
-    if (err) 
-      throw err;
-    res.send(JSON.stringify(user));
-  });
+  if (req.params.username) {
+    console.log('GET: game by game:' + req.params.username);
+    User.findOne({ username: req.params.username }, (err, user) => {
+      if (err)  throw err;
+      if (user == null)
+        res.send(JSON.stringify({ message: `The user "${req.params.username}" does not exist in the database` }));
+      else
+        res.send(JSON.stringify(user));
+    });
+  } else {
+    res.send(JSON.stringify({ message: "No user returned due to having no queries" }));
+  }
 })
 
 router.post('', (req, res, next) => {
   res.contentType("application/json");
-  console.log('UPDATE: User by username: ' + req.body.username);
-  let values = req.body;
-  if (values && Object.keys(values).length > 0) {
-    let user = User(values);
-    user.save((err, doc) => {
-      if (err)
-        throw err;
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = decodedToken.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.username) {
+          console.log('CREATE: User: ' + values.username);
+          let user = User(values);
+          user.save((err, doc) => {
+            if (err)
+              throw err;
 
-      console.log(`Created new user: ${values.username}`);
-      res.send(JSON.stringify(doc));
+            console.log(`Created new user: ${values.username}`);
+            res.send(JSON.stringify(doc));
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid CREATE Query for User" }));
+        }
+      }
     });
   } else {
-    res.send(JSON.stringify({ message: "Invalid CREATE Query for User" }));
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
   }
 })
 
 router.put('', (req, res, next) => {
   res.contentType("application/json");
-  console.log('UPDATE: User by : ' + req.body.username);
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = decodedToken.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.username) {
+          console.log('UPDATE: User by id: ' + values._id);
 
-  User.update({username: req.body.username}, (err, user) => {
-    console.log(`Updated ${user.username} from Users collection`);
-  });
+          User.findByIdAndUpdate(values._id, (err, user) => {
+            console.log(`Updated ${user.username} from Users collection`);
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid UPDATE Query for User" }));
+        }
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+  }
 })
 
 router.delete('', (req, res, next) => {
   res.contentType("application/json");
-  console.log('UPDATE: User by username: ' + req.body.username);
+  if (req.body.token) {
+    let token = req.body.token;
+    let decodedToken = Buffer.from(token, 'base64').toString();
+    decodedToken = decodedToken.split("#");
+    let account = JSON.parse(decodedToken[decodedToken.length-1]);
+    Account.findOne(account, (err, account) => {
+      if (account == null) {
+        res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+      } else {
+        let values = req.body;
+        if (values && Object.keys(values).length > 0 && values.username) {
+          console.log('DELETE: User by id:' + values._id);
+          console.log(values);
 
-  User.findOneAndRemove({username: req.body.username}, (err, user) => {
-    console.log(`Deleted ${user.username} from Users collection`);
-  });
+          User.findByIdAndDelete(values._id, (err) => {
+            console.log(`Deleted ${values.username} from Users collection`);
+          });
+        } else {
+          res.send(JSON.stringify({ message: "Invalid DELETE Query for User" }));
+        }
+      }
+    });
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Authorization Token" }));
+  }
 });
 
 module.exports = router;
