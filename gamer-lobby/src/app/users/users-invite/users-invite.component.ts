1b7063d90c4b00f13159b390f39c8a51ff99f9ec
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2018-12-07 14:51:56 -0500

    [Components][Users-Invite] Invite now updates user game & status

--- gamer-lobby/src/app/users/users-invite/users-invite.component.ts
+++ gamer-lobby/src/app/users/users-invite/users-invite.component.ts
@@ -4,6 +4,7 @@ import { GameService } from '../../services/game.service';
 import { UserService } from '../../services/user.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-users-invite',
@@ -14,7 +15,7 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./users-invite.component.css']
 })
 export class UsersInviteComponent implements OnInit {
-  user: User = new User();
+  user: any = new User();
 
   constructor(
     private userService: UserService, 
@@ -31,8 +32,20 @@ export class UsersInviteComponent implements OnInit {
     });
   }
 
-  invite(): void {
-    this.router.navigateByUrl('/users');
+  invite(form: NgForm): void {
+    let values = form.value;
+    values.status = "In Game";
+    values._id = this.user._id; values.__v = this.user.__v;
+    values.type = "invite";
+    let update = this.userService.update(values, false);
+    if (update) {
+      update.subscribe(response => {
+        if (response.json() && response.json().username) {
+          this.router.navigateByUrl("/admin");
+          this.userService.updateData();
+        }
+      });
+    }
   }
 
   cancel(): void {
